98183f649a99cdf69ff89da279e68c61b19f4472
OO6ana <analuizacoelhodemoura343@gmail.com>
2022-02-07 15:56:59 -0300

    commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// LAÇOS DE REPETIÇÕES - LOOPS
+
+// OPERADORES DE ATRIBUIÇÃO
+
+// "=" ATRIBUI UM VALOR
+// "+" ATRIBUI UMA CONCATENAÇÃO
+// "-" ATRIBUI UMA SUBTRAÇÃO
+// "/" ATRIBUI UMA DIVISÃO
+// "*" ATRIBUI UMA MULTIPLICAÇÃO
+
+// "++" ATRIBUI UM INCREMENTO
+// "--" ATRIBUI UM DECREMENTO
+
+// let X = 1
+// let Y = 2
+
+// console.log(X = X+Y) //completa
+
+// console.log(X = Y) // simplificada
+
+// console.log(2)
+// console.log(4)
+// console.log(6)
+// console.log(8)
+// console.log(10)
+// console.log(12)
+
+// LOOP WHILE
+
+/* 
+            !!ESTRUTURA DO WHILE!!
+
+VARIAVEL = VALOR
+WHILE (CONDIÇÃO){
+    TAREFA
+    INTERAÇÃO
+}
+*/
+
+// EXEMPLO
+
+// let contador = 1
+
+// while (1 >= contador){
+//     console.log(contador)
+//     contador ++
+// }
+
+
+//LOOP "FOR"
+
+// for(let contador = 2; contador <=12; contador +=2){
+//     console.log(contador)
+//}
+
+/* 
+        !!ESTRUTURA DO "FOR"!!
+
+FOR (VARIAVEL - LET - CONTADORA; CONDIÇÃO; - ";" É USADO PRA SEPARAR AS CONDIÇÕES - INTERAÇÃO){
+
+    CONSOLE - TAREFA
+}
+*/
+
+// for(let i = 1; i<=10; i ++){
+//     console.log(i)
+// }
+
+// for(let i = 1; i <= 10; i +=2){
+//     console.log(i)
+// }
